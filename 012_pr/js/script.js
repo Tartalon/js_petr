08a1ec5e6b6070fc79ba22400170b18c002eaad8
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-06-20 23:29:09 +0300

    18 use functions

--- 012_pr/js/script.js
+++ 012_pr/js/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
+let numberOfFilms;
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -10,12 +9,55 @@ const personalMovieDB = {
   privet: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", "");
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("done");
+    } else {
+      console.log("error");
+      i--;
+    }
+  }
+}
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Просмотренно маловато фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Классический зритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Киноман");
+  } else {
+    console.log("Ошибка");
+  }
+}
+
+function showMyDB(hidden) {
+  if (!hiden) {
+    console.log(personaMovieDB);
+  }
+}
+
+function wrightYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    const genre = prompt(`Ваш любимій жанр под номером ${i}`);
+    personalMovieDB.genres[i - 1] = genre;
+  }
+}
+
+wrightYourGenres();
+showMyDB(personalMovieDB.privet);
 
 console.log(personalMovieDB);
