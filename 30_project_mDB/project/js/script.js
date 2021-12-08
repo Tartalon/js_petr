97ec931c2e0fb9309feb58709099ccf32ff6ca75
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-12-08 16:21:30 +0200

    030_task done

--- 30_project_mDB/project/js/script.js
+++ 30_project_mDB/project/js/script.js
@@ -8,19 +8,57 @@
 Реализовать только при помощи JS
 
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+Отсортировать их по алфавиту
 
 5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+    'Логан',
+    'Лига справедливости',
+    'Ла-ла лэнд',
+    'Одержимость',
+    'Скотт Пилигрим против...',
+  ],
 };
 
+const promoAdv = document.querySelectorAll('.promo__adv img');
+const promoGenre = document.querySelector('.promo__genre');
+const promoBg = document.querySelector('.promo__bg');
+const promoInteractiveList = document.querySelector('.promo__interactive-list');
+
+function removeAdv(arr) {
+  arr.forEach(element => {
+    element.remove();
+  });
+}
+
+function changeText(block) {
+  block.textContent = 'драма';
+}
+
+function changeBg(block) {
+  block.style.backgroundImage = 'url(./img/bg.jpg)';
+}
+
+function displayMovieTitles(list) {
+  const sortedArr = movieDB.movies.sort();
+
+  list.innerHTML = '';
+
+  sortedArr.forEach((item, i) => {
+    list.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${sortedArr[i]}
+            <div class="delete"></div>
+        </li>
+      `;
+  });
+  console.log(sortedArr);
+}
+
+removeAdv(promoAdv);
+changeText(promoGenre);
+changeBg(promoBg);
+displayMovieTitles(promoInteractiveList);
