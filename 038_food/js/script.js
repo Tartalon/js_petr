05debc449ee626e64baab5e5b21b3e28d041c6aa
Vladimir <50200804+Tartalon@users.noreply.github.com>
2022-01-18 16:33:59 +0200

    slider

--- 038_food/js/script.js
+++ 038_food/js/script.js
@@ -195,34 +195,31 @@ window.addEventListener('DOMContentLoaded', () => {
 		return await res.json();
 	};
 
-	new MenuCard(
-		'img/tabs/elite.jpg',
-		'elite',
-		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		20,
-		'.menu .container'
-	).render();
+	// getResource('http://localhost:3000/menu').then(data => {
+	// data.forEach(({ img, altimg, title, descr, price }) => {
+	// 	new MenuCard(
+	// 		img,
+	// 		altimg,
+	// 		title,
+	// 		descr,
+	// 		price,
+	// 		'.menu .container'
+	// 	).render();
+	// 	});
+	// });
 
-	new MenuCard(
-		'img/tabs/vegy.jpg',
-		'vegy',
-		'Меню “Фитнес”',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-		9,
-		'.menu .container',
-		'menu__item'
-	).render();
-
-	new MenuCard(
-		'img/tabs/post.jpg',
-		'post',
-		'Меню “Постное”',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндал, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-		16,
-		'.menu .container',
-		'menu__item'
-	).render();
+	axios.get('http://localhost:3000/menu').then(data => {
+		data.data.forEach(({ img, altimg, title, descr, price }) => {
+			new MenuCard(
+				img,
+				altimg,
+				title,
+				descr,
+				price,
+				'.menu .container'
+			).render();
+		});
+	});
 
 	// Forms
 
@@ -307,4 +304,119 @@ window.addEventListener('DOMContentLoaded', () => {
 	// fetch('http://localhost:3000/menu')
 	// 	.then(data => data.json())
 	// 	.then(res => console.log(res));
+
+	// Slider
+
+	const slides = document.querySelectorAll('.offer__slide'),
+		prev = document.querySelector('.offer__slider-prev'),
+		next = document.querySelector('.offer__slider-next'),
+		total = document.querySelector('#total'),
+		current = document.querySelector('#current'),
+		slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+		slidesField = document.querySelector('.offer__slider-inner'),
+		width = window.getComputedStyle(slidesWrapper).width;
+
+	let slideIndex = 1;
+	let offset = 0;
+
+	if (slides.length < 10) {
+		total.textContent = `0${slides.length}`;
+		current.textContent = `0${slideIndex}`;
+	} else {
+		total.textContent = slides.length;
+		current.textContent = slideIndex;
+	}
+
+	slidesField.style.width = 100 * slides.length + '%';
+
+	slidesField.style.display = 'flex';
+	slidesField.style.transition = '.5s all';
+
+	slidesWrapper.style.overflow = 'hidden';
+
+	slides.forEach(slide => {
+		slide.style.width = width;
+	});
+
+	next.addEventListener('click', () => {
+		if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+			offset = 0;
+		} else {
+			offset += +width.slice(0, width.length - 2);
+		}
+		slidesField.style.transform = `translateX(-${offset}px)`;
+
+		if (slideIndex == slides.length) {
+			slideIndex = 1;
+		} else {
+			slideIndex++;
+		}
+
+		if (slides.length < 10) {
+			current.textContent = `0${slideIndex}`;
+		} else {
+			current.textContent = slideIndex;
+		}
+	});
+
+	prev.addEventListener('click', () => {
+		if (offset === 0) {
+			offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+		} else {
+			offset -= +width.slice(0, width.length - 2);
+		}
+		slidesField.style.transform = `translateX(-${offset}px)`;
+
+		if (slideIndex == 1) {
+			slideIndex = slides.length;
+		} else {
+			slideIndex--;
+		}
+
+		if (slides.length < 10) {
+			current.textContent = `0${slideIndex}`;
+		} else {
+			current.textContent = slideIndex;
+		}
+	});
+
+	// showSlides(slideIndex);
+
+	// if (slides.length < 10) {
+	// 	total.textContent = `0${slides.length}`;
+	// } else {
+	// 	total.textContent = slides.length;
+	// }
+
+	// function showSlides(n) {
+	// 	if (n > slides.length) {
+	// 		slideIndex = 1;
+	// 	}
+
+	// 	if (n < 1) {
+	// 		slideIndex = slides.length;
+	// 	}
+
+	// 	slides.forEach(item => (item.style.display = 'none'));
+
+	// 	slides[slideIndex - 1].style.display = 'block';
+
+	// 	if (slides.length < 10) {
+	// 		current.textContent = `0${slideIndex}`;
+	// 	} else {
+	// 		current.textContent = slideIndex;
+	// 	}
+	// }
+
+	// function plusSlides(n) {
+	// 	showSlides((slideIndex += n));
+	// }
+
+	// prev.addEventListener('click', e => {
+	// 	plusSlides(-1);
+	// });
+
+	// next.addEventListener('click', () => {
+	// 	plusSlides(1);
+	// });
 });
