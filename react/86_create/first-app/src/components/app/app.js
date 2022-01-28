e72d7351c52a3f8f052238cf3c9f0a0311f9fc21
Vladimir <50200804+Tartalon@users.noreply.github.com>
2022-01-28 14:16:27 +0200

    react props

--- react/86_create/first-app/src/components/app/app.js
+++ react/86_create/first-app/src/components/app/app.js
@@ -1,25 +1,31 @@
-import AppInfo from "../app-info/app-info";
-import SearchPanel from "../search-panel/search-panel";
-import AppFilter from "../app-filter/app-filter";
-import EmployeesList from "../employees-list/employees-list";
-import EmployeesAddForm from "../employees-add-form/employees-add-form";
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
-import "./app.css";
+import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-      <AppInfo />
+	const data = [
+		{ name: 'Jhon C.', salary: 800, increase: true, id: 1 },
+		{ name: 'Evon R.', salary: 1000, increase: false, id: 2 },
+		{ name: 'Dred W.', salary: 1500, increase: false, id: 3 },
+	];
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
-      </div>
+	return (
+		<div className="app">
+			<AppInfo />
 
-      <EmployeesList />
-      <EmployeesAddForm />
-    </div>
-  );
+			<div className="search-panel">
+				<SearchPanel />
+				<AppFilter />
+			</div>
+
+			<EmployeesList data={data} />
+			<EmployeesAddForm />
+		</div>
+	);
 }
 
 export default App;
