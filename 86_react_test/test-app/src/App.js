4ec9bb4e87ad6dc8879dc91376239d8a4bd6a06f
Vladimir <50200804+Tartalon@users.noreply.github.com>
2022-01-28 17:10:59 +0200

    sumthing

---
+++ 86_react_test/test-app/src/App.js
@@ -0,0 +1,44 @@
+import { Component } from 'react';
+
+import './App.css';
+
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 27,
+			text: 'Button',
+		};
+	}
+
+	nextYear = () => {
+		console.log('+++');
+		this.setState(state => ({
+			years: state.years + 1,
+		}));
+	};
+
+	render() {
+		const { name, surname, link } = this.props;
+		return (
+			<div>
+				<button onClick={this.nextYear}>{this.state.text}</button>
+				<h1>
+					My name is {name}, surname - {surname}, age - {this.state.years}
+				</h1>
+				<a href={link}>My profile</a>
+			</div>
+		);
+	}
+}
+
+function App() {
+	return (
+		<div className="App">
+			<WhoAmI name="Jhon" surname="Smith" link="facebook.com" />
+			<WhoAmI name="Bill" surname="Sheport" link="vk.com" />
+		</div>
+	);
+}
+
+export default App;
