c205a9a8f97d83d9e5e81b6f8de84b14bb5d4e6d
Vladimir <50200804+Tartalon@users.noreply.github.com>
2022-01-30 19:53:43 +0200

    flp

--- react/86_create/first-app/src/components/app/app.js
+++ react/86_create/first-app/src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,26 +8,37 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-	const data = [
-		{ name: 'Jhon C.', salary: 800, increase: true, id: 1 },
-		{ name: 'Evon R.', salary: 1000, increase: false, id: 2 },
-		{ name: 'Dred W.', salary: 1500, increase: false, id: 3 },
-	];
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{ name: 'Jhon C.', salary: 800, increase: true, id: 1 },
+				{ name: 'Evon R.', salary: 1000, increase: false, id: 2 },
+				{ name: 'Dred W.', salary: 1500, increase: false, id: 3 },
+			],
+		};
+	}
 
-	return (
-		<div className="app">
-			<AppInfo />
+	deleteItem = id => {
+		console.log(id);
+	};
 
-			<div className="search-panel">
-				<SearchPanel />
-				<AppFilter />
+	render() {
+		return (
+			<div className="app">
+				<AppInfo />
+
+				<div className="search-panel">
+					<SearchPanel />
+					<AppFilter />
+				</div>
+
+				<EmployeesList data={this.state.data} onDelete={this.deleteItem} />
+				<EmployeesAddForm />
 			</div>
-
-			<EmployeesList data={data} />
-			<EmployeesAddForm />
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
--- react/86_create/first-app/src/components/employees-list-item/employees-list-item.js
+++ react/86_create/first-app/src/components/employees-list-item/employees-list-item.js
@@ -24,7 +24,7 @@ class EmployeesListItem extends Component {
 	};
 
 	render() {
-		const { name, salary } = this.props;
+		const { name, salary, onDelete } = this.props;
 		const { increase } = this.state;
 		const { like } = this.state;
 
@@ -53,7 +53,7 @@ class EmployeesListItem extends Component {
 						<i className="fas fa-cookie"></i>
 					</button>
 
-					<button className="btn-trash btn-sm">
+					<button className="btn-trash btn-sm" onClick={onDelete}>
 						<i className="fas fa-trash"></i>
 					</button>
 					<i className="fas fa-star"></i>
